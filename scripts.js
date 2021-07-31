01376c3dfc6707b1fce4d7a225828526399e7ba6
Austin-axwell <austin.onyango@student.moringaschool.com>
2021-07-31 11:22:55 +0300

    added the variable to get total amount of cash

--- scripts.js
+++ scripts.js
@@ -3,3 +3,30 @@ function getSize() {
     var size = document.getElementById("pizzaSize").value;
     return parseInt(size);
   }
+  function getCrust() {
+    var crust = document.getElementById("pizzaCrust").value;
+    return parseInt(crust);
+  }
+  
+  function getTopping() {
+    var topping = document.getElementById("pizzaTopping").value;
+    return parseInt(topping);
+  }
+  function getQuantity() {
+    var number = document.getElementById("quantity").value;
+    return parseInt(number);
+  }
+  /*calculation of total amount*/
+  function totalAmount() {
+      var totals = (getSize() + getCrust() + getTopping()) * getQuantity();
+      alert(
+      "Your order of" +
+      getQuantity("") +
+      "Pizza," +
+      "" + "total to Shillings " +
+      totals +
+      "" + "" + "Keep Shopping with us."
+      );
+      /*location details*/
+    
+  }
